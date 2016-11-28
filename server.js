155ce52c963351eb7d560948a53c21e8daa0a53f
tiago.lima <tiago.lima>
2016-11-28 18:58:40 -0200

    Alterando mongolab

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var http = require('http')
   , app  = require('./config/app')();
 
 if(process.env.NODE_ENV == 'production'){
-  require('mongodb://tiago:admin@ds113668.mlab.com:13668/hallbeauty');
+  require('./config/database.js')('mongodb://tiago:admin@ds113668.mlab.com:13668/hallbeauty');
 }else{
   require('./config/database.js')('mongodb://localhost/hallbeauty');
 }
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Server listening in port ' + app.get('port'))
+  console.log('Server listening in port ' + app.get('port'));
 });
