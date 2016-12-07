5bceff0b63cbf6cf41f2f59fd1f48086741cdb32
tiago.lima <tiago.lima>
2016-12-07 16:40:22 -0200

    inserindo as datas

--- public/js/controllers/WeeksController.js
+++ public/js/controllers/WeeksController.js
@@ -16,8 +16,6 @@ angular.module('hallbeauty').controller('WeeksControllers', ['$scope','ServiceAp
     }
   ];
 
-  console.log($scope.datas[0].dias[0])
-
   $scope.selectSemana = function(semana){
     ServiceApi.getWeeks($scope.user).query(function(semanas){
       $scope.semana = semanas[semana];
