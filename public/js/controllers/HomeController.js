c71d6d20fd2b44f7310a1a3c2076416d846bc42e
tiago.lima <tiago.lima>
2016-12-07 16:37:45 -0200

    inserindo as datas

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -3,6 +3,17 @@ angular.module('hallbeauty').controller('HomeController', ['$scope','ServiceApi'
   $scope.semana = [];
   $scope.semanaSelected = 0;
   $scope.diaSelected;
+  $scope.datas = [
+    {
+      dias: ['12-12','13-12','14-12','15-12','16-12','17-12','18-12']
+    },
+    {
+      dias: ['19-12','20-12','21-12','22-12','23-12','24-12','25-12']
+    },
+    {
+      dias: ['26-12','27-12','28-12','29-12','30-12','31-12','01-01']
+    }
+  ];
 
 
   $scope.selectSemana = function(semana){
